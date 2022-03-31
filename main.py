2a0155a5f92cb977bb41700ec83e26847726b5c0
Fujiki <shinya.fujiki@gmail.com>
2022-03-31 22:40:41 +0900

    3st

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 
 st.title('straemlit入門')
 
 st.write('DataFrame')
-
-df = pd.DataFrame(
-    np.random.rand(15,3),
-    columns=['a','b','c']
-)
-
-st.write(df)
-
-st.table(df)
-
-st.dataframe(df, width=50, height=100)
-st.dataframe(df, width=100, height=100)
-
-st.line_chart(df)
